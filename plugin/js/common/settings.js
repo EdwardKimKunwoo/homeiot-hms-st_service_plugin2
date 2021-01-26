603b75aac6ae940b78429420ddbfa98d4817f727
cholhan-park <cholhan.park@samsung.com>
2021-01-26 13:21:37 +0900

    버전수정 1.1.7

--- plugin/js/common/settings.js
+++ plugin/js/common/settings.js
@@ -12,10 +12,10 @@ This software is the confidential and proprietary information of SAMSUNG ELECTRO
 
 /** plugin version 
  * @constant {string} */
-const VERSION = "1.1.5.1";
+const VERSION = "1.1.7";
 /** release date
  * @constant {string} */
-const RELEASE_DATE = "2021.01.30";
+const RELEASE_DATE = "2021.01.26";
 const CODE = "01";
 const PACKAGE = "HAS";
 /** API type
